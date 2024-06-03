5d75b4baefa1e26186d98083372b8400a4c7a88b
Vincent~ <vincenthleko95@gmail.com>
2024-06-03 13:47:20 +0200

    bootcamp-tests

---
+++ greet.js
@@ -0,0 +1,6 @@
+function greet (name) {
+    return "Hello, " + name;
+  }
+  
+  //const greet = name => `Hello, ${name}`
+  //console.log(greet ('Janine'));
---
+++ test/greet.tests.js
@@ -0,0 +1,9 @@
+describe('this test' , function(){
+    it('should show you how to use MochaJS' , function(){
+        assert.equal(greet("vincent"), "Hello, vincent");
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+});
+console.log(greet ("vincent"));
